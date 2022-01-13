6e518cf236d8c3d5db0bae5e044eda91d91058cd
Tokuda Hikaru <hkrtkd@gmail.com>
2022-01-13 22:46:15 +0900

    first commit

---
+++ android/app/src/main/kotlin/tokuda/maru_batsu_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package tokuda.maru_batsu_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(FontAwesomeIcons.circle, color: Colors.green, size: 30,),
+            Icon(Icons.clear, color: Colors.red,size: 30),
+            Text('ゲーム')
+          ],
+        ),
+      ),
+      body: buildField(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Column buildField() {
+    // 縦の3列を作成するためのリスト
+    List<Widget> _columnChildren = [];
+    // 横の3行を作成するためのリスト
+    List<Widget> _rowChildren = [];
+
+    for(int h = 0; h < 3; h++) {
+      // 横の3行を生成
+      for (int i = 0; i < 3; i++) {
+        _rowChildren.add(
+            Expanded(
+                child: AspectRatio(
+                    aspectRatio: 1.0,
+                    child: i == 2
+                        ? Container()
+                        : Row(
+                      children: [
+                        Expanded(child: Container()),
+                        VerticalDivider(width: 0.0, color: Colors.black,),
+                      ],
+                    )
+                )
+            )
+        );
+      }
+      _columnChildren.add(Row(children: _rowChildren,));
+      _columnChildren.add(Divider(height: 0.0, color: Colors.black,));
+      _rowChildren = [];
+    }
+    return Column(children: _columnChildren,);
+  }
+}
+
+//todo フィールドのUIを作成
+//todo フィールドのUI作成をメソッドｗお用いて簡潔に
+//todo ターンの表示とクリアボタンの作成
+//todo マス目をタップ可能にして、タップ時にターン切り替え
+//todo マス目をタップで〇✕を表示
+//todo ゲームの勝敗のパターンを書き出す
+//todo ゲームの勝敗を判定可能に
+//todo リセットボタンタップでリスタート可能に
